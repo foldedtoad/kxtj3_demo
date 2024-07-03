69378c0c5c42bd4f74f84e32173634e775e3e441
foldedtoad <foldedtoad@gmail.com>
2024-07-02 17:58:01 -0700

    dev check-in

--- boards/kxtj3_dt_symbols.h
+++ boards/kxtj3_dt_symbols.h
@@ -21,4 +21,29 @@
 #define KXTJ3_DT_ANYMOTION_AND_COMBINATION  2
 #define KXTJ3_DT_ANYMOTION_6D_POSITION      3
 
+/* Output Data Rate (ODR) in Hz */
+#define KXTJ3_DT_ODR_0p3905_HZ     0
+#define KXTJ3_DT_ODR_1p781_HZ      1
+#define KXTJ3_DT_ODR_1p563_HZ      2
+#define KXTJ3_DT_ODR_3p125_HZ      3
+#define KXTJ3_DT_ODR_6p25_HZ       4
+#define KXTJ3_DT_ODR_12p5_HZ       5
+#define KXTJ3_DT_ODR_25_HZ         6
+#define KXTJ3_DT_ODR_50_HZ         7
+#define KXTJ3_DT_ODR_100_HZ        8
+#define KXTJ3_DT_ODR_200_HZ        9
+#define KXTJ3_DT_ODR_400_HZ        10
+#define KXTJ3_DT_ODR_800_HZ        11
+
+/* Output Range in G units */
+#define KXTJ3_DT_ACCEL_RANGE_2G     0
+#define KXTJ3_DT_ACCEL_RANGE_4G     1
+#define KXTJ3_DT_ACCEL_RANGE_8G     2
+#define KXTJ3_DT_ACCEL_RANGE_16G    3
+
+/* Perfromance mode */
+#define KXTJ3_DT_OPER_MODE_LOW_POWER     0
+#define KXTJ3_DT_OPER_MODE_HIGH_RES      1
+#define KXTJ3_DT_OPER_MODE_HIGH_RES_16G  2
+
 #endif /* _KXTJ3_DT_SYMBOLS_H_ */
